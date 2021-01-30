e7ac4c245ecfcdedc8765ed8ccf327ce6d5c1b63
John Bede <johnbede2000@gmail.com>
2021-01-30 09:47:29 +0000

    nav link url data

---
+++ src/content/nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default [
+  { id: 1, text: 'Home', url: '/' },
+  { id: 2, text: 'News', url: '/news/' },
+  { id: 3, text: 'About', url: '/about/' },
+  { id: 4, text: 'Videos', url: '/videos/' },
+  { id: 5, text: 'Music', url: '/music/' },
+  { id: 6, text: 'Live', url: '/live/' },
+  { id: 7, text: 'Contact', url: '/contact/' },
+];

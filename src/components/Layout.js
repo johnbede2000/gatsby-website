04aea93b3cf71be8b231a192a5d636ae80d7e16d
John Bede <johnbede2000@gmail.com>
2021-01-31 12:18:43 +0000

    max width on layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,11 +4,11 @@ import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div className="width-container">
       <Nav />
       {children}
       <Footer />
-    </>
+    </div>
   );
 };
 

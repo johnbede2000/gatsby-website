7223bc34ba893d212b779d3bda7235824fba6e78
John Bede <johnbede2000@gmail.com>
2021-02-04 16:37:49 +0000

    footer in progress

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import { WidthContainer } from '../css/globalstyles';
 import styled from 'styled-components';
+import { FaSpotify, FaYoutube, FaApple } from 'react-icons/fa';
+import { ImSoundcloud } from 'react-icons/im';
 
 const year = new Date().getFullYear();
+
+const extLinks = [
+  {
+    id: 1,
+    icon: <FaSpotify />,
+    url:
+      'https://open.spotify.com/artist/64v43yzaVfp7hrdFDFKloJ?si=Em-oNQ6oQ8qyCDUIYWHmdg',
+  },
+  {
+    id: 2,
+    icon: <FaYoutube />,
+    url: 'https://www.youtube.com/channel/UCLSngNWnUNTttgex2q4lASg',
+  },
+  {
+    id: 3,
+    icon: <ImSoundcloud />,
+    url: 'https://soundcloud.com/johncervantes',
+  },
+  {
+    id: 4,
+    icon: <FaApple />,
+    url: 'https://music.apple.com/gb/artist/john-cervantes/1184527405',
+  },
+];
 
 // style
 const StripofColor = styled.div`
@@ -14,16 +40,37 @@ const StripofColor = styled.div`
   flex-shrink: 0;
 `;
 
+const ExternalLinks = styled.div`
+  display: block;
+  margin: 0 auto;
+  text-align: center;
+`;
+
+const Footerlink = styled.a`
+  font-size: 1.5rem;
+`;
+
+const Copyright = styled.p`
+  font-size: 0.75rem;
+  text-align: center;
+  margin-top: 0.5rem;
+`;
+
 // markup
 
 const Footer = () => {
   return (
     <StripofColor>
       <WidthContainer>
-        <p>
-          Copyright &copy; {year} John Cervantes &middot; Designed and Developed
-          by <a href="https://www.johnbe.de">John Bede</a> · All rights reserved
-        </p>
+        <ExternalLinks>
+          {extLinks.map((link) => {
+            return <Footerlink href={link.url}>{link.icon}</Footerlink>;
+          })}
+        </ExternalLinks>
+        <Copyright>
+          &copy; {year} John Cervantes &middot; Designed and Developed by{' '}
+          <a href="https://www.johnbe.de">John Bede</a> · All rights reserved
+        </Copyright>
       </WidthContainer>
     </StripofColor>
   );

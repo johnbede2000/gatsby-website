0fd97ce1348beb4aea6f6218a7b180867cd34b08
John Bede <johnbede2000@gmail.com>
2021-02-05 20:11:01 +0000

    footer added external links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { WidthContainer } from '../css/globalstyles';
 import styled from 'styled-components';
-import { FaSpotify, FaYoutube, FaApple } from 'react-icons/fa';
+import { FaSpotify, FaApple, FaAmazon } from 'react-icons/fa';
 import { ImSoundcloud } from 'react-icons/im';
+import { SiDeezer } from 'react-icons/si';
 
 const year = new Date().getFullYear();
 
@@ -15,16 +16,23 @@ const extLinks = [
   },
   {
     id: 2,
-    icon: <FaYoutube />,
-    url: 'https://www.youtube.com/channel/UCLSngNWnUNTttgex2q4lASg',
+    icon: <SiDeezer />,
+    url: 'https://www.deezer.com/en/artist/11527643',
   },
   {
     id: 3,
     icon: <ImSoundcloud />,
     url: 'https://soundcloud.com/johncervantes',
   },
+
   {
     id: 4,
+    icon: <FaAmazon />,
+    url: 'https://music.amazon.co.uk/artists/B01N7G6HFZ/john-cervantes',
+  },
+
+  {
+    id: 6,
     icon: <FaApple />,
     url: 'https://music.apple.com/gb/artist/john-cervantes/1184527405',
   },

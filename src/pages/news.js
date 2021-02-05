17a1bc246f1b7ab356160a708684cbdd5e090618
John Bede <johnbede2000@gmail.com>
2021-02-05 20:13:24 +0000

    holding news page

---
+++ src/pages/news.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const news = () => {
+  return (
+    <Layout curr="news">
+      <p>This site is under development. News will be added soon!</p>
+    </Layout>
+  );
+};
+
+export default news;

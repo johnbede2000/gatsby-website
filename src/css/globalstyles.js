be7cb4a1fd9e68dcc140eef2759443529ce1c805
John Bede <johnbede2000@gmail.com>
2021-02-05 20:12:39 +0000

    center div component

--- src/css/globalstyles.js
+++ src/css/globalstyles.js
@@ -5,3 +5,34 @@ export const WidthContainer = styled.div`
   width: 90vw;
   margin: 0 auto;
 `;
+
+export const Button = styled.a`
+  background: none;
+  color: white;
+  border-radius: 2px;
+  border: 1px solid white;
+  text-transform: uppercase;
+  text-align: center;
+  padding: 13px 10px;
+  display: block;
+  width: 100%;
+  max-width: 220px;
+  text-decoration: none;
+  transition: 0.2s background-color ease-out;
+
+  &:hover {
+    color: #121212;
+    background-color: white;
+  }
+`;
+
+export const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`;

93d087cdfc2080b8649964180a8dfb5b7e276d42
John Bede <johnbede2000@gmail.com>
2021-02-10 20:06:41 +0000

    holding music page

---
+++ src/components/MusicHolding.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MusicHolding = () => {
+  return <h2>Site under development, please check back soon!</h2>;
+};
+
+export default MusicHolding;
--- src/pages/music.js
+++ src/pages/music.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import Music from '../components/Music';
+// import Music from '../components/Music';
+import MusicHolding from '../components/MusicHolding';
 
 const music = () => {
   return (
     <Layout curr="music">
-      <Music />
+      <MusicHolding />
     </Layout>
   );
 };

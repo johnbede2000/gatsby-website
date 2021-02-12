31490700c4770a23e0e76c96cde75d4522f421e6
John Bede <johnbede2000@gmail.com>
2021-02-12 21:46:09 +0000

    styled component fix html tag

--- src/components/Formstyle.js
+++ src/components/Formstyle.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const Form = styled.div`
+const Form = styled.form`
   max-width: 35rem;
   background-color: var(--raise-one);
   padding: var(--vertical-gap) 1rem;

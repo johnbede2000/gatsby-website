3eacc4900fc82b1ea9d0a745494ad6e3953239ee
John Bede <johnbede2000@gmail.com>
2021-02-14 16:04:56 +0000

    pulling in contentful successfully

--- src/components/Infinite.js
+++ src/components/Infinite.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import Newsitem from '../components/Newsitem';
 
 const Infinite = () => {
-  return <div></div>;
+  const data = useStaticQuery(graphql`
+    {
+      allContentfulNews(sort: { fields: date, order: DESC }) {
+        nodes {
+          id
+          img {
+            fluid {
+              ...GatsbyContentfulFluid
+            }
+          }
+          date(formatString: "Do MMMM YYYY")
+          location
+          desc {
+            childMarkdownRemark {
+              html
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const {
+    allContentfulNews: { nodes: news },
+  } = data;
+  return (
+    <div>
+      {news.map((item) => {
+        return <Newsitem key={item.id} {...item}></Newsitem>;
+      })}
+    </div>
+  );
 };
 
 export default Infinite;
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Img from 'gatsby-image';
 
-const Newsitem = () => {
-  return <div></div>;
+const Newsitem = (item) => {
+  return (
+    <div>
+      <Img fluid={item.img.fluid} />
+      <p>
+        {item.date}, {item.location}
+      </p>
+      <div
+        dangerouslySetInnerHTML={{ __html: item.desc.childMarkdownRemark.html }}
+      />
+    </div>
+  );
 };
 
 export default Newsitem;

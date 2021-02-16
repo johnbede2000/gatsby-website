e89c1770c698fc3447e3128aa96881fe6d2e8324
John Bede <johnbede2000@gmail.com>
2021-02-16 07:03:51 +0000

    instagram data download

---
+++ src/content/instagram-data.js
@@ -0,0 +1,246 @@
+export const instagramData = [
+  {
+    caption: 'Last night in Maidstone with Katie Birtill',
+    taken_at: '2020-01-26T08:29:32+00:00',
+    location: 'Pizza Express Maidstone',
+    path: 'photos/202001/133ad267a1b8eef99c8e484c5748c984.jpg',
+  },
+  {
+    caption:
+      'Gigs coming up this weekend at Pizza Express Live with Katie Birtill - new arrangements of musical theatre songs, including two by myself, which Katie will be releasing soon!',
+    taken_at: '2020-01-07T11:15:32+00:00',
+    location: 'The Pheasantry Jazz Club, Chelsea London',
+    path: 'photos/202001/2a67f77741bb5f4d260661e988a7c94e.jpg',
+  },
+  {
+    caption: 'Christmas jazz',
+    taken_at: '2019-12-02T16:39:37+00:00',
+    path: 'photos/201912/53f16c0e234dcaca0c7461af46b7d1bc.jpg',
+  },
+  {
+    caption: '',
+    taken_at: '2019-11-28T00:38:38+00:00',
+    location: 'Scarfes Bar',
+    path: 'photos/201911/924ad034a99f04548568c5c018ccd892.jpg',
+  },
+  {
+    caption: '',
+    taken_at: '2019-11-26T23:44:11+00:00',
+    location: 'National Portrait Gallery',
+    path: 'photos/201911/49ebc3076108e4f24a0d087aee5b181b.jpg',
+  },
+  {
+    caption: 'With Leon Garner',
+    taken_at: '2019-11-13T23:57:53+00:00',
+    path: 'photos/201911/5219eb1a978e502ed9ba762a50e43908.jpg',
+  },
+  {
+    caption:
+      'What a pleasure playing on board 82.6m classic yacht Talitha for the Boat International Saint-Tropez Sundowners earlier this month. Entertainment provided by Blue International Talent & California Audio Technology #sttropez #superyacht #superyachtlife',
+    taken_at: '2019-10-18T10:39:25+00:00',
+    path: 'photos/201910/8dd9d2e4fde69f64ce3bcc1d69809ef6.jpg',
+  },
+  {
+    caption:
+      'Enjoyed playing with Palace Avenue Music yesterday featuring Louise',
+    taken_at: '2019-10-05T22:04:21+00:00',
+    path: 'photos/201910/c48fbc83922a3af449a5e36845e03601.jpg',
+  },
+  {
+    caption: 'Nice Sunday afternoon gig with Gabrielle Ducomble',
+    taken_at: '2019-09-29T21:15:34+00:00',
+    path: 'photos/201909/a52ad4ebdae988ba782edad6889eb6ac.jpg',
+  },
+  {
+    caption: 'With Leon Garner',
+    taken_at: '2019-09-22T08:57:36+00:00',
+    path: 'photos/201909/8aca97d20dfbc8d31c7c61436b1e7b16.jpg',
+  },
+  {
+    caption: '',
+    taken_at: '2019-08-28T11:18:21+00:00',
+    location: 'The Savoy',
+    path: 'photos/201908/ab6f8fdd075d3f33818c6d07cf764b5e.jpg',
+  },
+  {
+    caption: 'View from the stage at Twinwood Festival',
+    taken_at: '2019-08-25T01:42:41+00:00',
+    location: 'Twinwood Festival',
+    path: 'photos/201908/87ddc4ae79e4bc748da4fa39a985d6d2.jpg',
+  },
+  {
+    caption: '',
+    taken_at: '2019-07-31T00:37:46+00:00',
+    location: 'The Savoy',
+    path: 'photos/201907/3e4455defb2173b56575fd2e4e29212d.jpg',
+  },
+  {
+    caption: '',
+    taken_at: '2019-07-28T21:10:27+00:00',
+    location: 'Scarfes Bar',
+    path: 'photos/201907/dccf18a3e8857c55f97ced8ac168f98d.jpg',
+  },
+  {
+    caption: "I'm liking the balloons",
+    taken_at: '2019-05-09T10:42:20+00:00',
+    location: 'The Ned London',
+    path: 'photos/201905/881f2ed2e65e5695b7b692a40c1aa25d.jpg',
+  },
+  {
+    caption:
+      'Congratulations to Cherise Adams-Burnett on her Jazz FM Vocalist Of The Year award. I joined her yesterday to play a song at the end of an interview on BBC Radio London',
+    taken_at: '2019-05-03T22:24:13+00:00',
+    location: 'BBC Broadcasting House',
+    path: 'photos/201905/26500df25e06bd0f9a54111a35bee6fa.jpg',
+  },
+
+  {
+    caption: '',
+    taken_at: '2019-05-03T22:24:13+00:00',
+    location: 'BBC Broadcasting House',
+    path: 'photos/201905/11a9598c5af41612732cb71da4c37991.jpg',
+  },
+  {
+    caption:
+      "Tomorrow evening I'll be at Mill Hill Jazz Club with Katie Birtill from 8:30. Pictured here Pizza Express Live in Feb",
+    taken_at: '2019-04-09T13:51:45+00:00',
+    location: 'Pizza Express Live',
+    path: 'photos/201904/c50452bc44b35fd29d764e78eea943d9.jpg',
+  },
+  {
+    caption: 'Jazz in The City',
+    taken_at: '2019-04-05T16:31:17+00:00',
+    path: 'photos/201904/18e443ada55e12b7749be9d61491fe26.jpg',
+  },
+  {
+    caption: 'Jazz in The City',
+    taken_at: '2019-04-05T16:31:17+00:00',
+    path: 'photos/201904/848f5dcd02b82655846018cbb2bd1e65.jpg',
+  },
+  {
+    caption: 'Shooting a music video for Alfie Boe',
+    taken_at: '2019-01-23T13:38:00+00:00',
+    path: 'photos/201901/Screenshot.png',
+  },
+  {
+    caption:
+      "My stage tonight with Gareth Malone and the Royal Philharmonic Concert Orchestra. 21st & 22nd Dec we'll be at Union Chapel",
+    taken_at: '2018-12-09T20:51:40+00:00',
+    location: 'Alexandra Palace Theatre',
+    path: 'photos/201812/6c46370a24828fb8c4d2a6c6df30e16f.jpg',
+  },
+  {
+    caption: '',
+    taken_at: '2018-09-27T22:04:22+00:00',
+    location: 'Monaco',
+    path: 'photos/201809/11027c4da7b96e2819b223190dd1b5eb.jpg',
+  },
+  {
+    caption: 'A shot from last week at Glasgow Jazz Festival with Hailey Tuck',
+    taken_at: '2018-06-26T12:05:59+00:00',
+    location: 'Glasgow',
+    path: 'photos/201806/37637cd253241c2f159a676dfe53f247.jpg',
+  },
+  {
+    caption:
+      'Really enjoyed a gig this weekend playing swing music for dancers with Palace Avenue Music feat. Cherise Adams-Burnett for the Oxford Lindyhoppers',
+    taken_at: '2018-06-25T18:40:29+00:00',
+    location: 'Oxford Union Society Debating Chamber',
+    path: 'photos/201806/0109ff8d794c70024aeb0b9d4b1b40f7.jpg',
+  },
+  {
+    caption:
+      'Nice shot from last night with Aivale Cole & Mohammed Nazam and guest Benson Wilson',
+    taken_at: '2018-06-17T17:13:24+00:00',
+    location: 'The Musical Museum',
+    path: 'photos/201806/c7f0b55f5b34d1c924aeb0d29c163d15.jpg',
+  },
+  {
+    caption: '',
+    taken_at: '2018-04-03T14:52:29+00:00',
+    location: 'Hideaway Jazz Club',
+    path: 'photos/201804/a6324b39373f0920dd6498d07cb9fdfb.jpg',
+  },
+  {
+    caption: 'With Stella Angelika and Joe Downard',
+    taken_at: '2018-02-24T17:03:28+00:00',
+    path: 'photos/201802/cc08e3a968bbc13257043e904f808e75.jpg',
+  },
+  {
+    caption: 'With Shane Hampsheir',
+    taken_at: '2017-11-26T02:15:50+00:00',
+    path: 'photos/201711/c24ecd5321eaf4146d1db45d0a21a24f.jpg',
+  },
+  {
+    caption:
+      'Cherise Adams-Burnett with Harry Evans, Tom Farmer, Shane Forbes, Pete Beachill',
+    taken_at: '2017-11-26T02:02:21+00:00',
+    path: 'photos/201711/aa748a0fbde359b99fb977415854bb1c.jpg',
+  },
+  {
+    caption:
+      'With Steve Fishwick, Adam King, Lloyd Haines playing a rep from 1957',
+    taken_at: '2017-10-06T20:23:44+00:00',
+    path: 'photos/201710/e55af66576adbbd9788544b4343e3207.jpg',
+  },
+  {
+    caption: 'Photo shoot w/ James Allsopp, Fergus Ireland',
+    taken_at: '2017-06-28T07:56:38+00:00',
+    path: 'photos/201706/f8ec35a954f1a5920f8e34fc602e39ef.jpg',
+  },
+  {
+    caption: 'Jam session',
+    taken_at: '2017-06-17T23:34:42+00:00',
+    location: 'The Old Queens Head',
+    path: 'photos/201706/ed7e2dd866a97fb76bd99afc16aa04b8.jpg',
+  },
+  {
+    caption:
+      "This Sunday I'll be accompanying jazz singer Lauren Lucille at The Rose (Vauxhall) 14:00-16:00, and jazz singer Cherise Adams-Burnett at the Grape and Grain (Crystal Palace) 20:00-22:30 (alongside Henry Gilbert & Harry Sankey). Join for Sunday roast / real ales!",
+    taken_at: '2017-06-17T18:12:44+00:00',
+    location: 'The Grape and Grain',
+    path: 'photos/201706/58516990c7644d0022d1787b0ad7a0d3.jpg',
+  },
+  {
+    caption: 'Trio feat. Alex Garnett',
+    taken_at: '2017-05-03T23:19:08+00:00',
+    location: 'Stowe School',
+    path: 'photos/201705/9575e93579dba6b48a2371939339e185.jpg',
+  },
+  {
+    caption:
+      "I'll be leading a quartet at Arts at Stowe this Wednesday 3 May 8pm: feat. Alex Garnett sax, Nick Ereaut double bass, Dave Ingamells drums.",
+    taken_at: '2017-05-01T09:17:59+00:00',
+    location: 'Arts at Stowe',
+    path: 'photos/201705/b5a07ff4b70978e0231bd8392e6a4e86.jpg',
+  },
+  {
+    caption:
+      'Pre-release/launch CDs will be available to buy this Sunday! Buy tickets to the London Jazz Festival show',
+    taken_at: '2016-11-12T08:53:22+00:00',
+    location: 'London Jazz Festival',
+    path: 'photos/201611/2da2304c4e8d25ed669503fcbf5c4b62.jpg',
+  },
+  {
+    caption: 'A nice gig featuring fantastic vocalist Sara Dowling',
+    taken_at: '2016-09-22T22:39:38+00:00',
+    path: 'photos/201609/cf9ce81e8753298b86f39094e0ea5b5d.jpg',
+  },
+  {
+    caption: 'Skyline',
+    taken_at: '2016-09-09T21:56:48+00:00',
+    path: 'photos/201609/71ca0b6b8e56a8311d1feacf389dda3c.jpg',
+  },
+  {
+    caption: 'Solo piano',
+    taken_at: '2016-08-12T23:00:32+00:00',
+    location: 'The Gibson',
+    path: 'photos/201608/a37babc9047a0895c32183b5c0688c91.jpg',
+  },
+  {
+    caption: 'Paying tribute to Chet Baker this evening',
+    taken_at: '2016-07-16T16:19:48+00:00',
+    location: 'Bloomsbury Club Bar',
+    path: 'photos/201607/1c1f87f3c35e6ba22e32e4b915ebc378.jpg',
+  },
+];

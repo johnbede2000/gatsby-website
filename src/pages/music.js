d625660b95512c17ec1a810b2a3c2b4a2ab9c62c
John Bede <johnbede2000@gmail.com>
2021-02-18 20:52:54 +0000

    center div by vh

--- src/pages/music.js
+++ src/pages/music.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Layout from '../components/Layout';
 // import Music from '../components/Music';
 import MusicHolding from '../components/MusicHolding';
+import { Center } from '../css/globalstyles';
 
 const music = () => {
   return (
     <Layout curr="music">
-      <MusicHolding />
+      <Center style={{ minHeight: 'calc(100vh - 17rem)' }}>
+        <MusicHolding />
+      </Center>
     </Layout>
   );
 };
--- src/pages/success.js
+++ src/pages/success.js
@@ -10,7 +10,7 @@ const Htwo = styled.h2`
 const success = () => {
   return (
     <Layout curr="">
-      <Center>
+      <Center style={{ minHeight: 'calc(100vh - 17rem)' }}>
         <Htwo>Thank you for your message! I'll get back to you asap.</Htwo>
       </Center>
     </Layout>

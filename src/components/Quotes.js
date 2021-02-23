1c8467b9243680e35153e635f5d6a21656bcae2f
John Bede <johnbede2000@gmail.com>
2021-02-23 16:42:36 +0000

    quotes buttons added

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -40,17 +40,37 @@ const OneQuote = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
   width: 100%;
   background-color: var(--raise-one);
   border-radius: 10px;
   padding: 1em;
+  opacity: 0;
+
+  &.current {
+    opacity: 1;
+    transform: translateX(0);
+  }
+  &.prev {
+    transform: translateX(-100%);
+  }
+  &.next {
+    transform: translateX(100%);
+  }
+`;
+
+const FlexGrow = styled.div`
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Image = styled(Img)`
   border-radius: 50%;
   flex-shrink: 0;
-  margin-top: 1.5rem;
+  margin-top: 1rem;
 `;
 
 const Source = styled.p`
@@ -63,6 +83,28 @@ const Text = styled.p`
   margin-top: 1rem;
 `;
 
+const Dots = styled.div`
+  flex-shrink: 0;
+  flex-grow: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 1rem;
+
+  & div {
+    height: 15px;
+    width: 15px;
+    border-radius: 50%;
+    background-color: white;
+    opacity: 0.25;
+    cursor: pointer;
+    margin: 0 1rem;
+    &.active {
+      opacity: 1;
+    }
+  }
+`;
+
 // markup
 const Quotes = () => {
   const data = useStaticQuery(query);
@@ -73,20 +115,29 @@ const Quotes = () => {
   return (
     <Section>
       <OneQuote className={position}>
-        <svg
-          stroke="currentColor"
-          fill="currentColor"
-          opacity="0.2"
-          stroke-width="0"
-          viewBox="0 0 512 512"
-          height="3em"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"></path>
-        </svg>
-        <Text>{quotes[0].text}</Text>
-        <Image fixed={quotes[0].img.childImageSharp.fixed} />
-        <Source>— {quotes[0].source}</Source>
+        <FlexGrow>
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            opacity="0.2"
+            stroke-width="0"
+            viewBox="0 0 512 512"
+            height="3em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"></path>
+          </svg>
+          <Text>{quotes[0].text}</Text>
+          <Image fixed={quotes[0].img.childImageSharp.fixed} />
+          <Source>— {quotes[0].source}</Source>
+        </FlexGrow>
+        <Dots>
+          <div className="active"></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </Dots>
       </OneQuote>
     </Section>
   );

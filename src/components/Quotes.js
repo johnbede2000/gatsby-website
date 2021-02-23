8aaa073c3d01c9b931960496a9dc6bdff1ce407d
John Bede <johnbede2000@gmail.com>
2021-02-23 14:51:24 +0000

    updated quote layout

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -13,7 +13,7 @@ const query = graphql`
         text
         img {
           childImageSharp {
-            fixed(width: 100) {
+            fixed(width: 80) {
               ...GatsbyImageSharpFixed
             }
           }
@@ -25,11 +25,11 @@ const query = graphql`
 
 // styling
 const Section = styled.section`
-  padding-top: calc(var(--vertical-gap) * 2);
+  padding-top: var(--vertical-gap);
   position: relative;
   width: 100%;
-  height: 400px;
-  max-width: 400px;
+  height: 500px;
+  max-width: 300px;
   overflow: hidden;
   text-align: center;
   display: flex;
@@ -40,23 +40,23 @@ const OneQuote = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   width: 100%;
+  background-color: var(--raise-one);
+  border-radius: 10px;
+  padding: 1em;
 `;
 
 const Image = styled(Img)`
   border-radius: 50%;
+  flex-shrink: 0;
   margin-top: 1.5rem;
 `;
 
-const Svg = styled.svg`
-  position: absolute;
-  z-index: -1;
-  opacity: 0.1;
-`;
-
 const Source = styled.p`
-  font-style: italic;
+  font-weight: 600;
   margin-top: 1rem;
+  font-size: 1.25em;
 `;
 
 const Text = styled.p`
@@ -73,12 +73,17 @@ const Quotes = () => {
   return (
     <Section>
       <OneQuote className={position}>
-        <Svg width="40.83px" height="34.04px">
-          <path
-            d="M10.208 17.711h6.124v16.332H0V21.684C0 8.184 5.444.956 16.332 0v6.125c-4.083 1.14-6.124 4.414-6.124 9.82v1.766zm24.498 0h6.124v16.332H24.498V21.684C24.498 8.184 29.942.956 40.83 0v6.125c-4.083 1.14-6.124 4.414-6.124 9.82v1.766z"
-            fill="white"
-          ></path>
-        </Svg>
+        <svg
+          stroke="currentColor"
+          fill="currentColor"
+          opacity="0.2"
+          stroke-width="0"
+          viewBox="0 0 512 512"
+          height="3em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"></path>
+        </svg>
         <Text>{quotes[0].text}</Text>
         <Image fixed={quotes[0].img.childImageSharp.fixed} />
         <Source>— {quotes[0].source}</Source>
